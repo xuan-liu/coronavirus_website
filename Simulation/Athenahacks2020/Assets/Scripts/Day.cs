13a62af432c9d211f986bbf77c2d6770204b1cf1
jessicyz <jessicyz@usc.edu>
2020-03-08 08:13:11 -0700

    Added simulation so far

---
+++ Simulation/Athenahacks2020/Assets/Scripts/Day.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day : MonoBehaviour
+{
+    public int numAlive = 0;
+    public int numSick = 0;
+    public int numCorona = 0;
+    public int numCoronaPositive = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void nextDay(){
+
+    }
+
+    void calculatePersonStates(){
+        numAlive = Statistics.arr_people.Count;
+        numSick = 0;
+        numCorona = 0;
+        numCoronaPositive = 0;
+
+        for (int i = 0; i < Statistics.arr_people.Count; i++ ){
+            if (Statistics.arr_people[i].isDead){
+                numAlive--;
+            }
+            else{
+                if (Statistics.arr_people[i].visibleSymptoms){
+                    numSick++;
+                }
+
+                if (Statistics.arr_people[i].hasVirus){
+                    numCorona++;
+                    if (Statistics.arr_people[i].tested){
+                        numCoronaPositive++;
+                    }
+                }
+            }
+        }
+        
+    }
+
+}
---
+++ Simulation/Athenahacks2020/Assets/Scripts/Statistics.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Statistics
+{
+
+    public static float contagiousness = 0;
+
+    public static float deathRate = 0;
+    public static float washHandsReduction = 0;
+    public static float wearMasksReduction = 0;
+    public static float coronaRecoveryRate = 0;
+
+    public static float fluRecoveryRate = 0;
+
+    public static float globalChanceOfGoingToTest = 0;
+    public static int numPeople = 0;
+    public static List<Person> arr_people = new List<Person>();
+
+    public static List<Day> arr_day = new List<Day>();
+    public static int curDay = 0;
+
+
+    //If we have time, false negative and positive testing
+    public static float falseNegativeChance = 0;
+    public static float falsePositiveChance = 0;
+
+    //If we have time, separate by household
+    public static int maxNumPeoplePerHousehold = 6;
+    public static int maxNumHouseholds = 20;
+    public static int numHouseholds = 0;
+    public static List<Household> arr_households = new List<Household>();
+    static Statistics(){
+
+    }
+}
